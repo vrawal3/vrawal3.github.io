f7445377e52876e887cb851bb78795aceede4503
Yash Varma <yvarma2@uic.edu>
2022-11-26 16:49:59 -0600

    test3

--- bubble.js
+++ bubble.js
@@ -1,3 +1,7 @@
 // Copyright 2021 Observable, Inc.
 // Released under the ISC license.
 // https://observablehq.com/@d3/bubble-chart
+
+d3.csv("Vehicles.csv", function(data) {
+    console.log(data);
+});
