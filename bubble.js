7198b109c254523b7bda4575b36054acb0484432
vrawal3 <89661317+vrawal3@users.noreply.github.com>
2022-11-26 15:59:49 -0600

    1

---
+++ bubble.js
@@ -0,0 +1,7 @@
+// Copyright 2021 Observable, Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/bubble-chart
+
+const vehicles = './Vehicles.csv'
+var data = $.csv.toObjects(vehicles);
+console.log(data)
