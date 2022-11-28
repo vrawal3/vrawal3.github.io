d3af6a88679de92274f9c5c5c4cc4de715f0b279
vrawal3 <89661317+vrawal3@users.noreply.github.com>
2022-11-27 18:23:52 -0600

    1

--- weather.js
+++ weather.js
@@ -29,6 +29,37 @@ var yAxis;
 var svg;
 var height;
 
+var data11 = [
+    {group: "5", value: 4},
+    {group: "6", value: 16},
+    {group: "7", value: 8},
+];
+
+var data22 = [
+    {group: "8", value: 7},
+    {group: "9", value: 1},
+    {group: "10", value: 20},
+];
+
+var data33 = [
+    {group: "11", value: 10},
+    {group: "12", value: 4},
+    {group: "1", value: 14},
+];
+
+var data44 = [
+    {group: "2", value: 7},
+    {group: "3", value: 14},
+    {group: "4", value: 9},
+];
+
+var x1;
+var y1;
+var xAxis1;
+var yAxis1;
+var svg1;
+var height1;
+
 function bar1() {
     // create 2 data_set
     
@@ -92,12 +123,77 @@ function updateBar1(data) {
         .remove()
 }
 
+function bar2() {
+    // create 2 data_set
+    
+    // set the dimensions and margins of the graph
+    var margin = {top: 30, right: 30, bottom: 70, left: 60},
+        width = 460 - margin.left - margin.right;
+    height1 = 400 - margin.top - margin.bottom;
+    
+    // append the svg object to the body of the page
+    svg1 = d3.select("#my_dataviz1")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+    
+    // Initialize the X axis
+    x1 = d3.scaleBand()
+    .range([ 0, width ])
+    .padding(0.2);
+    xAxis1 = svg1.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    
+    // Initialize the Y axis
+    y1 = d3.scaleLinear()
+    .range([ height, 0]);
+    yAxis1 = svg1.append("g")
+    .attr("class", "myYaxis")
+    updateBar2(data1)
+}
+
+function updateBar2(data) {
+    // Update the X axis
+    x1.domain(data.map(function(d) { return d.group; }))
+    xAxis1.call(d3.axisBottom(x1))
+    
+    // Update the Y axis
+    y1.domain([0, d3.max(data, function(d) { return d.value }) ]);
+    yAxis1.transition().duration(1000).call(d3.axisLeft(y1));
+    
+    // Create the u variable
+    var u = svg1.selectAll("rect")
+        .data(data)
+    
+    u
+        .enter()
+        .append("rect") // Add a new rect for each new elements
+        .merge(u) // get the already existing elements as well
+        .transition() // and apply changes to all of them
+        .duration(1000)
+        .attr("x", function(d) { return x(d.group); })
+        .attr("y", function(d) { return y(d.value); })
+        .attr("width", x1.bandwidth())
+        .attr("height", function(d) { return height1 - y(d.value); })
+        .attr("fill", "#69b3a2")
+    
+    // If less group in the new dataset, I delete the ones not in use anymore
+    u
+        .exit()
+        .remove()
+}
+
 function update(data) {
+    updateBar1(data);
     updateBar1(data);
 }
 
 function init() {
     bar1();
+    bar2();
 }
 
 window.onload = init;
