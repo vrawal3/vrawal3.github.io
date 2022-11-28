c685888e996257f5e53490e079d8015d6801207f
vrawal3 <89661317+vrawal3@users.noreply.github.com>
2022-11-28 14:09:46 -0600

    1

---
+++ brushable.js
@@ -0,0 +1,107 @@
+var ageData = FileAttachment("sex@1.csv").csv({typed: true})
+var aData = ageData.map(d => ({
+    age: d['AGE'],
+    sex: d['SEX'],
+    ageFreq: d['AGE_FREQUENCY']
+  }))
+
+var margin = ({top: 10, right: 20, bottom: 50, left: 105});
+
+var visWidth = 400;
+var visHeight = 400;
+var sex = Array.from(new Set(aData.map(d => d.sex)));
+var barColor = d3.scaleOrdinal().domain(sex).range(d3.schemeCategory10);
+var x = d3.scaleLinear()
+        .domain(d3.extent(aData, d => d.age)).nice()
+        .range([0, visWidth])
+var y = d3.scaleLinear()
+        .domain(d3.extent(aData, d => d.ageFreq)).nice()
+        .range([visHeight, 0])
+
+function brushableScatterplot() {
+  // set up
+
+  // the value for when there is no brush
+  const initialValue = aData;
+
+  const svg = d3.select('#scatterPlot')
+      .append("svg")
+      .attr('width', visWidth + margin.left + margin.right)
+      .attr('height', visHeight + margin.top + margin.bottom+50)
+      .property('value', initialValue);
+
+  const g = svg.append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+  svg.append("text")
+     .attr("x", visWidth-100)
+     .attr("y", visHeight+90)
+     .attr("text-anchor", "middle")
+     .style("font-size", "30px")
+     .text("Frequency of Crashes by Age");
+  
+  // axes
+  g.append("g").call(xAxis, x, 'Age');
+  g.append("g").call(yAxis, y, 'Frequency');
+  
+  // draw points
+  
+  const radius = 3;
+  
+  const dots = g.selectAll('circle')
+    .data(aData)
+    .join('circle')
+      .attr('cx', d => x(d.age))
+      .attr('cy', d => y(d.ageFreq))
+      .attr('fill', d =>  barColor(d.sex))
+      .attr('opacity', 1)
+      .attr('r', radius);
+  
+  // ********** brushing here **********
+  
+  const brush = d3.brush()
+      // set the space that the brush can take up
+      .extent([[0, 0], [visWidth, visHeight]])
+      // handle events
+      .on('brush', onBrush)
+      .on('end', onEnd);
+  
+  g.append('g')
+      .call(brush);
+  
+  function onBrush(event) {
+    // event.selection gives us the coordinates of the
+    // top left and bottom right of the brush box
+    const [[x1, y1], [x2, y2]] = event.selection;
+    
+    // return true if the dot is in the brush box, false otherwise
+    function isBrushed(d) {
+      const cx = x(d.age);
+      const cy = y(d.ageFreq)
+      return cx >= x1 && cx <= x2 && cy >= y1 && cy <= y2;
+    } 
+    
+    // style the dots
+    dots.attr('fill', d => isBrushed(d) ? barColor(d.origin) : 'gray');
+    
+    // update the data that appears in the cars variable
+    svg.property('value', aData.filter(isBrushed)).dispatch('input');
+  }
+  
+  function onEnd(event) {
+    // if the brush is cleared
+    if (event.selection === null) {
+      // reset the color of all of the dots
+      dots.attr('fill', d => barColor(d.sex));
+      svg.property('value', initialValue).dispatch('input');
+    }
+  }
+
+  return svg.node();
+}
+
+function init() {
+    const scatter = brushableScatterplot();
+  }
+
+window.onload = init;
---
+++ brushableData.js
@@ -0,0 +1,992 @@
+var ageData = [
+    {
+      "AGE": 1,
+      "SEX": "F",
+      "AGE_FREQUENCY": 124
+    },
+    {
+      "AGE": 1,
+      "SEX": "M",
+      "AGE_FREQUENCY": 124
+    },
+    {
+      "AGE": 2,
+      "SEX": "F",
+      "AGE_FREQUENCY": 100
+    },
+    {
+      "AGE": 2,
+      "SEX": "M",
+      "AGE_FREQUENCY": 103
+    },
+    {
+      "AGE": 3,
+      "SEX": "F",
+      "AGE_FREQUENCY": 63
+    },
+    {
+      "AGE": 3,
+      "SEX": "M",
+      "AGE_FREQUENCY": 97
+    },
+    {
+      "AGE": 4,
+      "SEX": "F",
+      "AGE_FREQUENCY": 97
+    },
+    {
+      "AGE": 4,
+      "SEX": "M",
+      "AGE_FREQUENCY": 92
+    },
+    {
+      "AGE": 5,
+      "SEX": "F",
+      "AGE_FREQUENCY": 92
+    },
+    {
+      "AGE": 5,
+      "SEX": "M",
+      "AGE_FREQUENCY": 98
+    },
+    {
+      "AGE": 6,
+      "SEX": "F",
+      "AGE_FREQUENCY": 74
+    },
+    {
+      "AGE": 6,
+      "SEX": "M",
+      "AGE_FREQUENCY": 89
+    },
+    {
+      "AGE": 7,
+      "SEX": "F",
+      "AGE_FREQUENCY": 77
+    },
+    {
+      "AGE": 7,
+      "SEX": "M",
+      "AGE_FREQUENCY": 58
+    },
+    {
+      "AGE": 8,
+      "SEX": "F",
+      "AGE_FREQUENCY": 86
+    },
+    {
+      "AGE": 8,
+      "SEX": "M",
+      "AGE_FREQUENCY": 80
+    },
+    {
+      "AGE": 9,
+      "SEX": "F",
+      "AGE_FREQUENCY": 64
+    },
+    {
+      "AGE": 9,
+      "SEX": "M",
+      "AGE_FREQUENCY": 75
+    },
+    {
+      "AGE": 10,
+      "SEX": "F",
+      "AGE_FREQUENCY": 69
+    },
+    {
+      "AGE": 10,
+      "SEX": "M",
+      "AGE_FREQUENCY": 76
+    },
+    {
+      "AGE": 11,
+      "SEX": "F",
+      "AGE_FREQUENCY": 69
+    },
+    {
+      "AGE": 11,
+      "SEX": "M",
+      "AGE_FREQUENCY": 66
+    },
+    {
+      "AGE": 12,
+      "SEX": "F",
+      "AGE_FREQUENCY": 85
+    },
+    {
+      "AGE": 12,
+      "SEX": "M",
+      "AGE_FREQUENCY": 101
+    },
+    {
+      "AGE": 13,
+      "SEX": "F",
+      "AGE_FREQUENCY": 80
+    },
+    {
+      "AGE": 13,
+      "SEX": "M",
+      "AGE_FREQUENCY": 120
+    },
+    {
+      "AGE": 14,
+      "SEX": "F",
+      "AGE_FREQUENCY": 97
+    },
+    {
+      "AGE": 14,
+      "SEX": "M",
+      "AGE_FREQUENCY": 125
+    },
+    {
+      "AGE": 15,
+      "SEX": "F",
+      "AGE_FREQUENCY": 147
+    },
+    {
+      "AGE": 15,
+      "SEX": "M",
+      "AGE_FREQUENCY": 196
+    },
+    {
+      "AGE": 16,
+      "SEX": "F",
+      "AGE_FREQUENCY": 614
+    },
+    {
+      "AGE": 16,
+      "SEX": "M",
+      "AGE_FREQUENCY": 708
+    },
+    {
+      "AGE": 17,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1283
+    },
+    {
+      "AGE": 17,
+      "SEX": "M",
+      "AGE_FREQUENCY": 1676
+    },
+    {
+      "AGE": 18,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2373
+    },
+    {
+      "AGE": 18,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3232
+    },
+    {
+      "AGE": 19,
+      "SEX": "F",
+      "AGE_FREQUENCY": 3282
+    },
+    {
+      "AGE": 19,
+      "SEX": "M",
+      "AGE_FREQUENCY": 4393
+    },
+    {
+      "AGE": 20,
+      "SEX": "F",
+      "AGE_FREQUENCY": 3694
+    },
+    {
+      "AGE": 20,
+      "SEX": "M",
+      "AGE_FREQUENCY": 4777
+    },
+    {
+      "AGE": 21,
+      "SEX": "F",
+      "AGE_FREQUENCY": 4221
+    },
+    {
+      "AGE": 21,
+      "SEX": "M",
+      "AGE_FREQUENCY": 5014
+    },
+    {
+      "AGE": 22,
+      "SEX": "F",
+      "AGE_FREQUENCY": 4538
+    },
+    {
+      "AGE": 22,
+      "SEX": "M",
+      "AGE_FREQUENCY": 5368
+    },
+    {
+      "AGE": 23,
+      "SEX": "F",
+      "AGE_FREQUENCY": 4952
+    },
+    {
+      "AGE": 23,
+      "SEX": "M",
+      "AGE_FREQUENCY": 5653
+    },
+    {
+      "AGE": 24,
+      "SEX": "F",
+      "AGE_FREQUENCY": 5013
+    },
+    {
+      "AGE": 24,
+      "SEX": "M",
+      "AGE_FREQUENCY": 5880
+    },
+    {
+      "AGE": 25,
+      "SEX": "F",
+      "AGE_FREQUENCY": 5302
+    },
+    {
+      "AGE": 25,
+      "SEX": "M",
+      "AGE_FREQUENCY": 6255
+    },
+    {
+      "AGE": 26,
+      "SEX": "F",
+      "AGE_FREQUENCY": 5150
+    },
+    {
+      "AGE": 26,
+      "SEX": "M",
+      "AGE_FREQUENCY": 6297
+    },
+    {
+      "AGE": 27,
+      "SEX": "F",
+      "AGE_FREQUENCY": 5062
+    },
+    {
+      "AGE": 27,
+      "SEX": "M",
+      "AGE_FREQUENCY": 6285
+    },
+    {
+      "AGE": 28,
+      "SEX": "F",
+      "AGE_FREQUENCY": 4967
+    },
+    {
+      "AGE": 28,
+      "SEX": "M",
+      "AGE_FREQUENCY": 6247
+    },
+    {
+      "AGE": 29,
+      "SEX": "F",
+      "AGE_FREQUENCY": 4866
+    },
+    {
+      "AGE": 29,
+      "SEX": "M",
+      "AGE_FREQUENCY": 6169
+    },
+    {
+      "AGE": 30,
+      "SEX": "F",
+      "AGE_FREQUENCY": 4444
+    },
+    {
+      "AGE": 30,
+      "SEX": "M",
+      "AGE_FREQUENCY": 6098
+    },
+    {
+      "AGE": 31,
+      "SEX": "F",
+      "AGE_FREQUENCY": 4180
+    },
+    {
+      "AGE": 31,
+      "SEX": "M",
+      "AGE_FREQUENCY": 5789
+    },
+    {
+      "AGE": 32,
+      "SEX": "F",
+      "AGE_FREQUENCY": 3964
+    },
+    {
+      "AGE": 32,
+      "SEX": "M",
+      "AGE_FREQUENCY": 5586
+    },
+    {
+      "AGE": 33,
+      "SEX": "F",
+      "AGE_FREQUENCY": 3744
+    },
+    {
+      "AGE": 33,
+      "SEX": "M",
+      "AGE_FREQUENCY": 5536
+    },
+    {
+      "AGE": 34,
+      "SEX": "F",
+      "AGE_FREQUENCY": 3464
+    },
+    {
+      "AGE": 34,
+      "SEX": "M",
+      "AGE_FREQUENCY": 5122
+    },
+    {
+      "AGE": 35,
+      "SEX": "F",
+      "AGE_FREQUENCY": 3430
+    },
+    {
+      "AGE": 35,
+      "SEX": "M",
+      "AGE_FREQUENCY": 5041
+    },
+    {
+      "AGE": 36,
+      "SEX": "F",
+      "AGE_FREQUENCY": 3127
+    },
+    {
+      "AGE": 36,
+      "SEX": "M",
+      "AGE_FREQUENCY": 4877
+    },
+    {
+      "AGE": 37,
+      "SEX": "F",
+      "AGE_FREQUENCY": 3129
+    },
+    {
+      "AGE": 37,
+      "SEX": "M",
+      "AGE_FREQUENCY": 4526
+    },
+    {
+      "AGE": 38,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2961
+    },
+    {
+      "AGE": 38,
+      "SEX": "M",
+      "AGE_FREQUENCY": 4519
+    },
+    {
+      "AGE": 39,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2943
+    },
+    {
+      "AGE": 39,
+      "SEX": "M",
+      "AGE_FREQUENCY": 4466
+    },
+    {
+      "AGE": 40,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2925
+    },
+    {
+      "AGE": 40,
+      "SEX": "M",
+      "AGE_FREQUENCY": 4561
+    },
+    {
+      "AGE": 41,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2954
+    },
+    {
+      "AGE": 41,
+      "SEX": "M",
+      "AGE_FREQUENCY": 4169
+    },
+    {
+      "AGE": 42,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2660
+    },
+    {
+      "AGE": 42,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3971
+    },
+    {
+      "AGE": 43,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2477
+    },
+    {
+      "AGE": 43,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3853
+    },
+    {
+      "AGE": 44,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2265
+    },
+    {
+      "AGE": 44,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3918
+    },
+    {
+      "AGE": 45,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2287
+    },
+    {
+      "AGE": 45,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3610
+    },
+    {
+      "AGE": 46,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2174
+    },
+    {
+      "AGE": 46,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3652
+    },
+    {
+      "AGE": 47,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2109
+    },
+    {
+      "AGE": 47,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3515
+    },
+    {
+      "AGE": 48,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2226
+    },
+    {
+      "AGE": 48,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3728
+    },
+    {
+      "AGE": 49,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2310
+    },
+    {
+      "AGE": 49,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3879
+    },
+    {
+      "AGE": 50,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2149
+    },
+    {
+      "AGE": 50,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3764
+    },
+    {
+      "AGE": 51,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1911
+    },
+    {
+      "AGE": 51,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3588
+    },
+    {
+      "AGE": 52,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1899
+    },
+    {
+      "AGE": 52,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3435
+    },
+    {
+      "AGE": 53,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1814
+    },
+    {
+      "AGE": 53,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3395
+    },
+    {
+      "AGE": 54,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1817
+    },
+    {
+      "AGE": 54,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3490
+    },
+    {
+      "AGE": 55,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1888
+    },
+    {
+      "AGE": 55,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3392
+    },
+    {
+      "AGE": 56,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1774
+    },
+    {
+      "AGE": 56,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3337
+    },
+    {
+      "AGE": 57,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1726
+    },
+    {
+      "AGE": 57,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3263
+    },
+    {
+      "AGE": 58,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1611
+    },
+    {
+      "AGE": 58,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3222
+    },
+    {
+      "AGE": 59,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1533
+    },
+    {
+      "AGE": 59,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3105
+    },
+    {
+      "AGE": 60,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1546
+    },
+    {
+      "AGE": 60,
+      "SEX": "M",
+      "AGE_FREQUENCY": 3113
+    },
+    {
+      "AGE": 61,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1443
+    },
+    {
+      "AGE": 61,
+      "SEX": "M",
+      "AGE_FREQUENCY": 2770
+    },
+    {
+      "AGE": 62,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1360
+    },
+    {
+      "AGE": 62,
+      "SEX": "M",
+      "AGE_FREQUENCY": 2568
+    },
+    {
+      "AGE": 63,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1317
+    },
+    {
+      "AGE": 63,
+      "SEX": "M",
+      "AGE_FREQUENCY": 2319
+    },
+    {
+      "AGE": 64,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1191
+    },
+    {
+      "AGE": 64,
+      "SEX": "M",
+      "AGE_FREQUENCY": 2318
+    },
+    {
+      "AGE": 65,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1144
+    },
+    {
+      "AGE": 65,
+      "SEX": "M",
+      "AGE_FREQUENCY": 2059
+    },
+    {
+      "AGE": 66,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1011
+    },
+    {
+      "AGE": 66,
+      "SEX": "M",
+      "AGE_FREQUENCY": 1843
+    },
+    {
+      "AGE": 67,
+      "SEX": "F",
+      "AGE_FREQUENCY": 972
+    },
+    {
+      "AGE": 67,
+      "SEX": "M",
+      "AGE_FREQUENCY": 1574
+    },
+    {
+      "AGE": 68,
+      "SEX": "F",
+      "AGE_FREQUENCY": 775
+    },
+    {
+      "AGE": 68,
+      "SEX": "M",
+      "AGE_FREQUENCY": 1413
+    },
+    {
+      "AGE": 69,
+      "SEX": "F",
+      "AGE_FREQUENCY": 756
+    },
+    {
+      "AGE": 69,
+      "SEX": "M",
+      "AGE_FREQUENCY": 1349
+    },
+    {
+      "AGE": 70,
+      "SEX": "F",
+      "AGE_FREQUENCY": 738
+    },
+    {
+      "AGE": 70,
+      "SEX": "M",
+      "AGE_FREQUENCY": 1243
+    },
+    {
+      "AGE": 71,
+      "SEX": "F",
+      "AGE_FREQUENCY": 661
+    },
+    {
+      "AGE": 71,
+      "SEX": "M",
+      "AGE_FREQUENCY": 1112
+    },
+    {
+      "AGE": 72,
+      "SEX": "F",
+      "AGE_FREQUENCY": 605
+    },
+    {
+      "AGE": 72,
+      "SEX": "M",
+      "AGE_FREQUENCY": 1031
+    },
+    {
+      "AGE": 73,
+      "SEX": "F",
+      "AGE_FREQUENCY": 498
+    },
+    {
+      "AGE": 73,
+      "SEX": "M",
+      "AGE_FREQUENCY": 914
+    },
+    {
+      "AGE": 74,
+      "SEX": "F",
+      "AGE_FREQUENCY": 467
+    },
+    {
+      "AGE": 74,
+      "SEX": "M",
+      "AGE_FREQUENCY": 716
+    },
+    {
+      "AGE": 75,
+      "SEX": "F",
+      "AGE_FREQUENCY": 389
+    },
+    {
+      "AGE": 75,
+      "SEX": "M",
+      "AGE_FREQUENCY": 636
+    },
+    {
+      "AGE": 76,
+      "SEX": "F",
+      "AGE_FREQUENCY": 435
+    },
+    {
+      "AGE": 76,
+      "SEX": "M",
+      "AGE_FREQUENCY": 547
+    },
+    {
+      "AGE": 77,
+      "SEX": "F",
+      "AGE_FREQUENCY": 382
+    },
+    {
+      "AGE": 77,
+      "SEX": "M",
+      "AGE_FREQUENCY": 459
+    },
+    {
+      "AGE": 78,
+      "SEX": "F",
+      "AGE_FREQUENCY": 320
+    },
+    {
+      "AGE": 78,
+      "SEX": "M",
+      "AGE_FREQUENCY": 473
+    },
+    {
+      "AGE": 79,
+      "SEX": "F",
+      "AGE_FREQUENCY": 277
+    },
+    {
+      "AGE": 79,
+      "SEX": "M",
+      "AGE_FREQUENCY": 445
+    },
+    {
+      "AGE": 80,
+      "SEX": "F",
+      "AGE_FREQUENCY": 255
+    },
+    {
+      "AGE": 80,
+      "SEX": "M",
+      "AGE_FREQUENCY": 343
+    },
+    {
+      "AGE": 81,
+      "SEX": "F",
+      "AGE_FREQUENCY": 205
+    },
+    {
+      "AGE": 81,
+      "SEX": "M",
+      "AGE_FREQUENCY": 302
+    },
+    {
+      "AGE": 82,
+      "SEX": "F",
+      "AGE_FREQUENCY": 169
+    },
+    {
+      "AGE": 82,
+      "SEX": "M",
+      "AGE_FREQUENCY": 230
+    },
+    {
+      "AGE": 83,
+      "SEX": "F",
+      "AGE_FREQUENCY": 138
+    },
+    {
+      "AGE": 83,
+      "SEX": "M",
+      "AGE_FREQUENCY": 217
+    },
+    {
+      "AGE": 84,
+      "SEX": "F",
+      "AGE_FREQUENCY": 122
+    },
+    {
+      "AGE": 84,
+      "SEX": "M",
+      "AGE_FREQUENCY": 184
+    },
+    {
+      "AGE": 85,
+      "SEX": "F",
+      "AGE_FREQUENCY": 85
+    },
+    {
+      "AGE": 85,
+      "SEX": "M",
+      "AGE_FREQUENCY": 158
+    },
+    {
+      "AGE": 86,
+      "SEX": "F",
+      "AGE_FREQUENCY": 76
+    },
+    {
+      "AGE": 86,
+      "SEX": "M",
+      "AGE_FREQUENCY": 146
+    },
+    {
+      "AGE": 87,
+      "SEX": "F",
+      "AGE_FREQUENCY": 86
+    },
+    {
+      "AGE": 87,
+      "SEX": "M",
+      "AGE_FREQUENCY": 104
+    },
+    {
+      "AGE": 88,
+      "SEX": "F",
+      "AGE_FREQUENCY": 46
+    },
+    {
+      "AGE": 88,
+      "SEX": "M",
+      "AGE_FREQUENCY": 67
+    },
+    {
+      "AGE": 89,
+      "SEX": "F",
+      "AGE_FREQUENCY": 35
+    },
+    {
+      "AGE": 89,
+      "SEX": "M",
+      "AGE_FREQUENCY": 63
+    },
+    {
+      "AGE": 90,
+      "SEX": "F",
+      "AGE_FREQUENCY": 49
+    },
+    {
+      "AGE": 90,
+      "SEX": "M",
+      "AGE_FREQUENCY": 49
+    },
+    {
+      "AGE": 91,
+      "SEX": "F",
+      "AGE_FREQUENCY": 28
+    },
+    {
+      "AGE": 91,
+      "SEX": "M",
+      "AGE_FREQUENCY": 35
+    },
+    {
+      "AGE": 92,
+      "SEX": "F",
+      "AGE_FREQUENCY": 14
+    },
+    {
+      "AGE": 92,
+      "SEX": "M",
+      "AGE_FREQUENCY": 40
+    },
+    {
+      "AGE": 93,
+      "SEX": "F",
+      "AGE_FREQUENCY": 19
+    },
+    {
+      "AGE": 93,
+      "SEX": "M",
+      "AGE_FREQUENCY": 16
+    },
+    {
+      "AGE": 94,
+      "SEX": "F",
+      "AGE_FREQUENCY": 8
+    },
+    {
+      "AGE": 94,
+      "SEX": "M",
+      "AGE_FREQUENCY": 22
+    },
+    {
+      "AGE": 95,
+      "SEX": "F",
+      "AGE_FREQUENCY": 11
+    },
+    {
+      "AGE": 95,
+      "SEX": "M",
+      "AGE_FREQUENCY": 13
+    },
+    {
+      "AGE": 96,
+      "SEX": "F",
+      "AGE_FREQUENCY": 1
+    },
+    {
+      "AGE": 96,
+      "SEX": "M",
+      "AGE_FREQUENCY": 4
+    },
+    {
+      "AGE": 97,
+      "SEX": "F",
+      "AGE_FREQUENCY": 2
+    },
+    {
+      "AGE": 98,
+      "SEX": "F",
+      "AGE_FREQUENCY": 5
+    },
+    {
+      "AGE": 98,
+      "SEX": "M",
+      "AGE_FREQUENCY": 8
+    },
+    {
+      "AGE": 99,
+      "SEX": "F",
+      "AGE_FREQUENCY": 4
+    },
+    {
+      "AGE": 99,
+      "SEX": "M",
+      "AGE_FREQUENCY": 2
+    },
+    {
+      "AGE": 100,
+      "SEX": "M",
+      "AGE_FREQUENCY": 7
+    }
+   ]
