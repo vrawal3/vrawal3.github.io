66e7bd3a729931018fe0d19d09595979f585d644
vrawal3 <89661317+vrawal3@users.noreply.github.com>
2022-11-28 23:12:31 -0600

    Update weatherData.js

--- weatherData.js
+++ weatherData.js
@@ -1,25 +1,25 @@
 var data1 = [
-    {group: "5", value: 9527},
-    {group: "6", value: 10709},
-    {group: "7", value: 11179},
+    {group: "May", value: 9527},
+    {group: "June", value: 10709},
+    {group: "July", value: 11179},
 ];
 
 var data2 = [
-    {group: "8", value: 11433},
-    {group: "9", value: 5838},
-    {group: "10", value: 6078},
+    {group: "August", value: 11433},
+    {group: "September", value: 5838},
+    {group: "October", value: 6078},
 ];
 
 var data3 = [
-    {group: "11", value: 5172},
-    {group: "12", value: 4287},
-    {group: "1", value: 9178},
+    {group: "November", value: 5172},
+    {group: "December", value: 4287},
+    {group: "January", value: 9178},
 ];
 
 var data4 = [
-    {group: "2", value: 10418},
-    {group: "3", value: 8498},
-    {group: "4", value: 7445},
+    {group: "February", value: 10418},
+    {group: "March", value: 8498},
+    {group: "April", value: 7445},
 ];
 
 var ata1 = [
